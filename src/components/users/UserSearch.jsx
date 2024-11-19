16c8c13e414781455ff8ab56c57c583db85bcd65
Aqil321821 <aqilrao321821@gmail.com>
2024-11-19 21:25:29 +0500

    install axios and make use of it and use promise.all() to make two requests

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,30 +1,36 @@
 import { useState, useContext } from 'react';
+
 import GithubContext from '../../context/github/GithubContext';
 import AlertContext from '../../context/alert/AlertContext';
+import { searchUsers } from '../../context/github/GithubActions';
 
 function UserSearch() {
   const [text, setText] = useState('');
 
-  const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { users, dispatch } = useContext(GithubContext);
   const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => {
     setText(e.target.value);
   };
 
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (text === '') {
       setAlert('Enter Something to Serach', 'error');
     } else {
-      //@todo-search users
-      searchUsers(text);
-      setText('');
+      try {
+        dispatch({ type: 'SET_LOADING' });
+        //@todo-search users
+        const users = await searchUsers(text);
+        dispatch({ type: 'GET_USERS', payload: users });
+        setText('');
+      } catch (error) {
+        setAlert('Failed to fetch users', error);
+      }
     }
   };
 
-  
   return (
     <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
       <div>
@@ -41,7 +47,7 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-          <button onClick={clearUsers} className='btn btn-ghost btn-lg'>
+          <button onClick={() => dispatch({ type: 'CLEAR_USERS' })} className='btn btn-ghost btn-lg'>
             CLEAR
           </button>
         </div>
